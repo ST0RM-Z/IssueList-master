9dc79223b27142b35dc747ecb05e59008ed496fe
Dhairya Arya <stromcyclone1921@gmail.com>
2022-09-26 19:02:30 -0400

    graphql

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-const { ApolloServer } = require("apollo-server-express");
 const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+
 
 const app = express();
 const PORT = 3000;
+
+
 // ---------------graphql -------------------
     const typeDefs = `
       type Query {
@@ -12,6 +15,30 @@ const PORT = 3000;
         setAboutMessage(message: String!): String
       }
     `;
+const aboutMessage = "hello i am just developer";
+function setAboutMessage(_, { message }) {
+  return aboutMessage = message;
+}
+    const resolvers = {
+      Query: {
+        about: () => aboutMessage,
+      },
+      Mutation: {
+        setAboutMessage,
+      },
+    };
+
+   
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+server.start().then(()=>{
+  server.applyMiddleware({ app, path: '/graphql',cors:true });
+
+})
+
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
